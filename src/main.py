1c7d7fa4df16eda2bb246167bd4cd131197bb298
Heverton <hb.hevertonborges@gmail.com>
2026-04-23 19:58:49 -0300

    Update scan_automaton function to use Automaton class

--- src/main.py
+++ src/main.py
@@ -1,41 +1,70 @@
 import sys
 
+
+class Automaton:
+    __states = {}
+    __alphabet = []
+
+    def __init__(self, num_states, alphabet):
+        for i in range(num_states):
+            self.__states[f"q{i + 1}"] = []
+
+        if type(alphabet) is list:
+            self.__alphabet = alphabet
+        elif type(alphabet) is str:
+            token = ""
+
+            for c in alphabet:
+                if c != "," and c != "\n":
+                    token += c
+                else:
+                    self.__alphabet.append(token)
+                    token = ""
+
+    def set_transition(self, qi, qj, character):
+        if qi not in self.__states.keys() or qj not in self.__states.keys():
+            print(f"Estados {qi} e {qj} não pertencem ao autômato")
+            return
+
+        if character not in self.__alphabet:
+            print(f"Caractere {character} não pertence ao alfabeto do autômato")
+            return
+
+        self.__states[qi].append((character, qj))
+
+    def get_states(self):
+        return self.__states.keys()
+
+    def get_alphabet(self):
+        return self.__alphabet
+
+    def string(self):
+        automaton_str = ""
+
+        for state, transactions in self.__states.items():
+            automaton_str += state + ":\n    "
+
+            for t in transactions:
+                automaton_str += f"δ({state}, {t[0]}) = {t[1]}  "
+
+            automaton_str += "\n"
+
+        return automaton_str
+
+
 def scan_automaton():
-    print('Digite os estados do autômato separados por vírgula:')
+    num_states = int(input("Digite o número de estados do autômato: "))
+    print()
 
-    states_str = sys.stdin.readline()
-    states = []
-    token = ''
+    print("Digite os caracteres do alfabeto separados por vírgula:\n    ", end="")
+    automaton = Automaton(num_states, sys.stdin.readline())
+    print()
 
-    for i in states_str:
-        if i != ',' and i != '\n':
-            token += i
-        else:
-            states.append(token) 
-            token = ''
+    print("Defina as transições correspondentes:")
 
-    print('Digite os caracteres do alfabeto separados por vírgula:')
+    for i in automaton.get_states():
+        for j in automaton.get_alphabet():
+            automaton.set_transition(i, input(f"    δ({i}, {j}) = "), j)
+    print()
 
-    characteres_str = sys.stdin.readline()
-    characteres = []
-
-    for i in characteres_str:
-        if i != ',' and i != '\n':
-            token += i
-        else:
-            characteres.append(token) 
-            token = ''
-
-    print('Informe as transições correspondentes:')
-
-    automaton = {}
-
-    for i in states:
-        automaton[i] = []
-        for j in characteres:
-            state = input(f"δ({i}, {j}) = ")
-            automaton[i].append((j, state))
-
-    print(automaton)
-    
-scan_automaton()
+    return automaton
